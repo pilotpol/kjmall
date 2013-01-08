05f1d7cd6ad2564e319fa7e0ebeac0145d8e088a
pilotpol <pilot.polathep@gmail.com>
2013-01-08 23:40:16 +0700

    first commited

---
+++ application/views/mainmaster.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>:: Welcome to KJ Mall v.1.0::</title>
+
+</head>
+<body>
+    
+    <div id="mainHeader">
+        
+        <div id="Logo">
+        </div>
+        <div id="Head-space">
+        
+        </div>
+        
+        <div id="Login-panel">
+        </div>
+        
+        <div id="topNavi">
+        </div>
+        
+    </div>
+    
+    <div id="mainContain">
+        <div id="left-contain">
+        
+            <div id="mainBanner">
+            </div>
+            
+            <div id="left-subcontain">
+                
+                <div id="left-subcontain-panel">
+                    
+                    <div id="short-navi">
+                    </div>
+                    
+                    <div id="categories-panel">
+                    </div>
+                    
+                </div>
+                
+                <div id="right-subcontain-panel">
+                    
+                    <div id="update-news-panel">
+                    </div>
+                    
+                    <div id="recomment-panel">
+                    </div>
+                    
+                    <div id="webbord-panel">
+                    </div>
+                    
+                </div>
+                
+                
+            </div>
+        
+        </div>
+        
+        <div id="right-contain">
+            
+            <div id="welcome-panel">
+            </div>
+            
+            <div id="social-panel">
+            </div>
+            
+            <div id="review-panel">
+            </div>
+            
+        </div>
+        
+    </div>
+<script type="text/javascript" src="<?php echo base_url(); ?>asset/js/jquery.js"></script>
+</body>
+</html>
